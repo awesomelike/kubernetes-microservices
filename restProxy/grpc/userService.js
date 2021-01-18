c1ccd3cf710aaa653c6d86199249e0b20ee014ad
awesomelike <abdullaev.dev@gmail.com>
2021-01-19 02:17:39 +0500

    fix k8s configs

--- restProxy/grpc/userService.js
+++ restProxy/grpc/userService.js
@@ -16,9 +16,4 @@ const { UserService } = userPackage;
 
 let client = new UserService('0.0.0.0:50051', grpc.credentials.createInsecure());
 
-// client.register({name:"Azamat", email: "azamat@gmail.com", password: "123456"}, (err, res) => {console.log(err);
-// console.log});
-
-// client.login({email: "azamat@gmail.com", password: "123456"}, (e, r) => {if (e) console.log(e); console.log(r)})
-// client.auth({token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImE4MWU2YTVmLWM1MmItNGZmMC05OTRlLThkNjVmNGQ4MmZjMCIsImVtYWlsIjoiYXphbWF0QGdtYWlsLmNvbSIsImlhdCI6MTYxMDk5OTMyNCwiZXhwIjoxNjExMDAyOTI0fQ.ckGvHbMINUm6BKmKfgWhunpGsaCngG3306wHBr5osg8"}, (e,r) => console.log(r))
 module.exports = client;
