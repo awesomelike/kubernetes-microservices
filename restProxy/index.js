afc6e8247ff6804c7e4bbb71a8a88e78d66dabc1
awesomelike <abdullaev.dev@gmail.com>
2021-01-22 03:52:59 +0500

    add metrics server

--- restProxy/index.js
+++ restProxy/index.js
@@ -9,7 +9,15 @@ app.use(express.urlencoded({ extended: false }));
 indexRouter(app);
 
 const port = process.env.PORT || 3000; 
-
+app.get('/', (req, res) => {
+  res.sendStatus(200);
+  while(true) {
+    for (let i=1; i<1000; i++) {
+      const x = Math.random();
+      console.log('INFINITE!');
+    }
+  }
+})
 app.listen(port, () => {
   console.log("Server started!");
 })
